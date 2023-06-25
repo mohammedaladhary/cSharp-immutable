6e60c4c28997ce39198a8c7a35adfacdba20022f
mohammedaladhary <mohammed.aladhary@outlook.com>
2023-06-25 14:10:12 -0700

    search number in an array

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace c__Immutable
+{
+    internal class Program
+    {
+        static bool SearchNumber(int[] arr, int target)
+        {
+            foreach (int num in arr)
+            {
+                if (num == target)
+                    return true;
+                else if (num > target)
+                    return false;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+                int[] numbers = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+                int targetNumber = 11;
+
+            if (SearchNumber(numbers, targetNumber))
+                Console.WriteLine("Number found in the array.");
+            else
+                Console.WriteLine("Number not found in the array.");
+        }
+    }
+}
